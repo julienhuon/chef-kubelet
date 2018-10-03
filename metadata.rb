627560687a7dd164376870a262dca179d81b0560
Julien Huon <julien@huon.email>
2018-10-03 17:33:34 +0200

    New version : 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'julien@huon.email'
 license 'Apache-2.0'
 description 'Installs/Configures kubelet'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 14.4.56' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/julienhuon/chef-kubelet/issues'
